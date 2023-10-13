ff27035c9fe3eaff7e57215726eb94f9033f9668
mizuki <banana0822hairuyo@gmail.com>
2023-10-13 22:12:19 +0900

    2023/10/13 22:12

---
+++ album/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from .views import PhotoListView, PhotoDetailView, TagPhotoListView
+
+
+urlpatterns = [
+    path("", PhotoListView.as_view(), name="photo-list"),
+    path("tag/<str:tag>/", TagPhotoListView.as_view(), name="tag-photo-list"),
+    path("photo/<int:pk>", PhotoDetailView.as_view(), name="photo-detail"),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
